3053f3f502c0aef860b2ce0f9089453f50d6a162
JadenLund <lundjaden@gmail.com>
2023-03-23 13:12:31 -0500

    Adding in the square function to clean up the board HTML code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from "react";
 
-export default function Square() {
-  return (<button className="square">X</button>)
+function Square() {
+  <button className="square" onClick={handleClick}> </button>
+}
+
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
 }
