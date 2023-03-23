d6122923abc31b8d71bd06ed5ea59b6a9395bd93
JadenLund <lundjaden@gmail.com>
2023-03-23 14:08:09 -0500

    adding in the calculate winners

--- src/App.js
+++ src/App.js
@@ -20,6 +20,27 @@ export default function Board() {
     setXIsNext(!xIsNext)
   }
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+
   //variable = expression1? expression2 : expression3
 
   //fills all of the squares with the value null
